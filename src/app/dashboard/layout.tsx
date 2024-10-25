c40f8c4025e809dd74faf588839cde91de1f0a15
Rajat Gupta <rajatgupta0607@gmail.com>
2024-10-25 16:23:08 +0530

    RBAC integration with previous routes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,11 +3,14 @@
 import Dashboard from "@/components/Dashboard/Dashboard";
 import { withAuth } from "@/components/WithAuth";
 import { EmployeesProvider } from "@/context/EmployeesContext";
+import { RolesProvider } from "@/context/RolesContext";
 
 function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<EmployeesProvider>
-			<Dashboard>{children}</Dashboard>
+			<RolesProvider>
+				<Dashboard>{children}</Dashboard>
+			</RolesProvider>
 		</EmployeesProvider>
 	);
 }
--- src/app/dashboard/role/page.tsx
+++ src/app/dashboard/role/page.tsx
@@ -5,23 +5,20 @@ import { useSession } from "next-auth/react";
 import { Group } from "@mantine/core";
 import RolesTable from "@/components/RolesTable";
 import CreateRoleForm from "@/components/CreateRoleForm";
-import { RolesProvider } from "@/context/RolesContext";
 
 export default function Main() {
 	const { data: session } = useSession();
 
 	return (
-		<RolesProvider>
-			<div>
-				{session?.user.sAdmin && (
-					<Group justify="flex-end">
-						<ModalContainer title="Add Role">
-							<CreateRoleForm />
-						</ModalContainer>
-					</Group>
-				)}
-				<RolesTable />
-			</div>
-		</RolesProvider>
+		<div>
+			{session?.user.sAdmin && (
+				<Group justify="flex-end">
+					<ModalContainer title="Add Role">
+						<CreateRoleForm />
+					</ModalContainer>
+				</Group>
+			)}
+			<RolesTable />
+		</div>
 	);
 }
